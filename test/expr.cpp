99ade267351e3a60e1069cc9dd9977294ceef9f6
Gerry Candy <g3candy@gmail.com>
2020-06-18 17:14:48 +0100

    numerical bs test

--- test/expr.cpp
+++ test/expr.cpp
@@ -29,6 +29,24 @@ TEST(Expr,ExogenousSymbol){
         EXPECT_EQ(0.0, std::reinterpret_pointer_cast<Constant>(ds_dt)->Value() );
 }
 
+TEST(Expr,EndgenousSymbol){
+
+        auto sub_expr = Constant::Make(2.0);
+
+        auto sym = "Foo";
+        auto s = EndgenousSymbol::Make(sym, sub_expr);
+        EXPECT_EQ(OPKind_EndgenousSymbol, s->Kind()  );
+        EXPECT_EQ(sym, std::reinterpret_pointer_cast<EndgenousSymbol>(s)->Name() );
+
+        auto ds_ds = s->Diff(sym);
+        auto ds_dt = s->Diff("Other");
+
+        EXPECT_EQ(OPKind_Constant, ds_ds->Kind());
+        EXPECT_EQ(1.0, std::reinterpret_pointer_cast<Constant>(ds_ds)->Value() );
+        EXPECT_EQ(OPKind_Constant, ds_dt->Kind());
+        EXPECT_EQ(0.0, std::reinterpret_pointer_cast<Constant>(ds_dt)->Value() );
+}
+
 /*
         From Savine
  */
@@ -74,7 +92,7 @@ TEST(Black,Numeric){
                                 EndgenousSymbol::Make("K", K)
                         )
                 ),
-                std
+                EndgenousSymbol::Make("std", std)
         );
 
         auto d1 = BinaryOperator::Add(
@@ -160,6 +178,84 @@ TEST(Black,Numeric){
 
         double c_value = df_value * ( F_value * nd1_value - K_value * nd2_value );
         EXPECT_FLOAT_EQ(c_value, c->Eval(ST));
+        
+        struct RemoveEndo : OperatorTransform{
+                virtual std::shared_ptr<Operator> Apply(std::shared_ptr<Operator> const& ptr){
+                        auto candidate = ptr->Clone(shared_from_this());
+                        if( candidate->Kind() == OPKind_EndgenousSymbol ){
+                                if( auto typed = std::dynamic_pointer_cast<EndgenousSymbol>(candidate)){
+                                        return typed->Expr();
+                                }
+                        }
+                        return candidate;
+                }
+        };
+        auto c_raw = c->Clone(std::make_shared<RemoveEndo>());
+
+        EXPECT_FLOAT_EQ(c->Eval(ST), c_raw->Eval(ST));
+
+        double epsilon = 0.00001;
+        auto baseline = c->Eval(ST);
+        auto bump_ST = ST;
+        bump_ST("sig", sig_value + epsilon);
+        auto sig_bump = c->Eval(bump_ST);
+        auto fd_d_sig = ( sig_bump - baseline ) / epsilon;
+        std::cout << "sig_bump => " << sig_bump << "\n"; // __CandyPrint__(cxx-print-scalar,sig_bump)
+        std::cout << "fd_d_sig => " << fd_d_sig << "\n"; // __CandyPrint__(cxx-print-scalar,fd_d_sig)
+        std::cout << "c_raw->Diff(\"sig\")->Eval(ST) => " << c_raw->Diff("sig")->Eval(ST) << "\n"; // __CandyPrint__(cxx-print-scalar,c_raw->Diff("sig")->Eval(ST))
+
+
+        double adj_S0  = 0.0;
+        double adj_r   = 0.0;
+        double adj_y   = 0.0;
+        double adj_sig = 0.0;
+        double adj_K   = 0.0;
+        double adj_T   = 0.0;
+        double adj_df  = 0.0;
+        double adj_F   = 0.0;
+        double adj_std = 0.0;
+        double adj_d   = 0.0;
+        double adj_d1  = 0.0;
+        double adj_d2  = 0.0;
+        double adj_nd1 = 0.0;
+        double adj_nd2 = 0.0;
+        double adj_c   = 1.0;
+
+        EXPECT_FLOAT_EQ(F_value * nd1_value - K_value * nd2_value, c->Diff("df")->Eval(ST));
+        EXPECT_FLOAT_EQ(df_value * nd1_value, c->Diff("F")->Eval(ST));
+        EXPECT_FLOAT_EQ(df_value * F_value, c->Diff("nd1")->Eval(ST));
+        EXPECT_FLOAT_EQ(-df_value * nd2_value, c->Diff("K")->Eval(ST));
+        EXPECT_FLOAT_EQ(-df_value * K_value, c->Diff("nd2")->Eval(ST));
+
+        adj_df   +=  adj_c * c->Diff("df")->Eval(ST);
+        adj_F    +=  adj_c * c->Diff("F") ->Eval(ST);
+        adj_nd1  +=  adj_c * c->Diff("nd1") ->Eval(ST);
+        adj_K    +=  adj_c * c->Diff("K") ->Eval(ST);
+        adj_nd2  +=  adj_c * c->Diff("nd2") ->Eval(ST);
+
+        adj_d1   += adj_nd1 * nd1->Diff("d1")->Eval(ST);
+        adj_d2   += adj_nd2 * nd2->Diff("d2")->Eval(ST);
+
+        adj_d    += adj_d1 * d1->Diff("d1")->Eval(ST);
+        adj_std  += adj_d1 * d1->Diff("std")->Eval(ST);
+        adj_d    += adj_d2 * d2->Diff("d2")->Eval(ST);
+        adj_std  += adj_d2 * d2->Diff("std")->Eval(ST);
+
+        adj_F    += adj_d * d->Diff("F")->Eval(ST);
+        adj_K    += adj_d * d->Diff("K")->Eval(ST);
+        adj_std  += adj_d * d->Diff("std")->Eval(ST);
+
+        adj_sig  += adj_std * std->Diff("sig")->Eval(ST);
+
+        std::cout << "adj_sig => " << adj_sig << "\n"; // __CandyPrint__(cxx-print-scalar,adj_sig)
+
+
+
+
+
+
+
+        
 
                                 
                         
